b2ac197fe79a23d7101a4f4abaf672ad0b1b9278
HOANG LINH BUI <100017631@student.swin.edu.au>
2019-12-07 15:54:46 +1100

    Added action command popup trigger

--- Robot Rising Revengeance/Assets/Scripts/HUDCanvas.cs
+++ Robot Rising Revengeance/Assets/Scripts/HUDCanvas.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDCanvas : MonoBehaviour
 {
-    public GameObject dialogObjectPrefab;
+    public GameObject dialogBoxObjectPrefab;
+    public GameObject actionPopupObjPrefab;
 
+    private GameObject currentActionPopup;
+    private GameObject currentDialogBox;
+
+    private Camera cam;
+    private RectTransform canvasRect;
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = Camera.main;
+        canvasRect = this.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -18,8 +26,33 @@ public class HUDCanvas : MonoBehaviour
         
     }
 
+    public void CreateActionPopup(GameEvent e)
+    {
+       
+        DestroyPopup();
+
+        currentActionPopup = Instantiate(actionPopupObjPrefab, this.transform);
+        RectTransform r = currentActionPopup.GetComponent<RectTransform>();
+        Vector2 newPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect,cam.WorldToScreenPoint(e.transform.position) + e.popupOffset, null, out newPos);
+        Debug.Log(r.localPosition);
+        newPos.x = Mathf.Clamp(newPos.x, -1980/2 + r.sizeDelta.x / 2, 1980/2 - r.sizeDelta.x / 2);
+        newPos.y = Mathf.Clamp(newPos.y, -1080/2 + r.sizeDelta.y / 2,  1080/2 - r.sizeDelta.y / 2);
+        r.localPosition = newPos;
+        currentActionPopup.transform.Find("ActionText").GetComponent<Text>().text = e.command;
+    }
+
     public void CreateDialogBox(GameEvent e)
     {
+    
+    }
 
+
+    public void DestroyPopup()
+    {
+        if (currentActionPopup != null)
+        {
+            Destroy(currentActionPopup);
+        }
     }
 }
