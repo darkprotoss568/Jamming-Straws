bcc47383c7d30f6598f0d97ac00228edf075f2f3
HOANG LINH BUI <100017631@student.swin.edu.au>
2019-12-07 11:14:07 +1100

    Added base player control codes

---
+++ Robot Rising Revengeance/Assets/Scripts/ControlScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScript : MonoBehaviour
+{
+    public bool normalMovementUnlocked = false;
+    private Transform playerTransform;
+    public float maxSpeed;
+
+    public float rotateSpeed;
+    private Vector3 currentDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = gameObject.transform;
+        currentDirection = new Vector3(0, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleControls();
+    }
+
+    private void HandleControls()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        if (!normalMovementUnlocked)
+        {
+            Debug.DrawRay(playerTransform.position, currentDirection, Color.green);
+
+            if (hor > 0)
+            {
+                currentDirection = Quaternion.Euler(0, 0, rotateSpeed) * currentDirection;
+            } else if (hor < 0)
+            {
+
+            }
+        }
+
+    }
+}
+
