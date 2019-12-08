daae8dc3021fd779db546de18926c7fb7a404d9e
HOANG LINH BUI <100017631@student.swin.edu.au>
2019-12-08 13:31:29 +1100

    Added controllable door

---
+++ Robot Rising Revengeance/Assets/Scripts/DoorOpenEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenEvent : GameEvent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasBeenActivated = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Activate()
+    {
+        gameObject.GetComponent<Animator>().SetTrigger("Open");
+    }
+
+}
