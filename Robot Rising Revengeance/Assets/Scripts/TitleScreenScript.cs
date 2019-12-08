cbaca10014fd663f234c54d42f7f65f9f45e7fda
HOANG LINH BUI <100017631@student.swin.edu.au>
2019-12-08 13:04:58 +1100

    Fixed UI button highlight

---
+++ Robot Rising Revengeance/Assets/Scripts/TitleScreenScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleScreenScript : MonoBehaviour
+{
+    public AudioSource soundEffects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlaySoundEffect(AudioClip clip)
+    {
+        soundEffects.Stop();
+        soundEffects.PlayOneShot(clip);
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
